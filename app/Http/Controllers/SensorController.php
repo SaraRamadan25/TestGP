4e71b89bf1ccd4c60bc81cc6e77b3e1f0cfe301e
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2024-01-23 22:45:13 +0200

    work on facebook login

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers;
 use App\Events\SensorDataReceived;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SensorController extends Controller
 {
     public function getData(Request $request): JsonResponse
     {
+        Log::info('Received sensor data', $request->all());
+
         // Get the HeartRate and Spo2 query parameters
         $heartRate = $request->query('HeartRate');
         $spo2 = $request->query('Spo2');
