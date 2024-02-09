51c7ce06b6c133f98b23940d624a3d47e974dedb
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2024-02-09 17:12:27 +0200

    Update

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,12 @@ use App\Http\Controllers\API\HeartRateController;
 use App\Http\Controllers\API\JacketController;
 use App\Http\Controllers\API\LocationController;
 use App\Http\Controllers\API\SensorController;
-use App\Http\Controllers\API\SocialiteAuthController;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FaceBookController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QrcodeController;
+use App\Http\Controllers\SocialiteAuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,6 +38,11 @@ Route::post('forgot',  [AuthController::class, 'forgot'])->name('password.forgot
 Route::post('reset', [AuthController::class, 'reset'])->name('password.reset');
 Route::apiResource('users', UserController::class)->except('index');
 
+// Login with 3rd part
+
+Route::get('/auth/redirect/github', [SocialiteAuthController::class, 'redirectToGitHub'])->middleware('web');
+Route::get('/auth/callback/github', [SocialiteAuthController::class, 'handleGitHubCallback'])->middleware('web');
+
 Route::get('/',[HomeController::class,'index'])->name('home');
 
 
@@ -49,9 +54,6 @@ Route::post('/sensor-data', [SensorController::class, 'sensorData']);
 Route::get('/heart-rate', [HeartRateController::class, 'getHeartRate']);
 
 Route::post('/positionstack-api',[LocationController::class, 'positionStack'])->name('location.api');
-Route::get('/positionstack', [LocationController::class, 'positionStack']);
-
-Route::post('/arcgis-api',[LocationController::class, 'arcgis'])->name('weather.api');
 
 
 Route::get('/check/{modelno}', [JacketController::class, 'check'])->name('check');
@@ -59,13 +61,12 @@ Route::get('/share-qrcode', [QrcodeController::class, 'shareQRCode'])->name('sha
 
 Route::post('/scan-jacket', [JacketController::class, 'scanJacket'])->name('scan.jacket');
 
-Route::get('/auth/redirect/github', [SocialiteAuthController::class, 'redirectToGitHub'])->middleware('web');
-Route::get('/auth/callback/github', [SocialiteAuthController::class, 'handleGitHubCallback'])->middleware('web');
+
 
 Route::get('login/facebook', [FaceBookController::class, 'redirectToFacebook'])->name('login.facebook')->middleware('web');
 Route::get('login/facebook/callback', [FaceBookController::class, 'handleFacebookCallback'])->middleware('web');
 
-Route::post('submit-health-data',[HealthController::class,'submitHealthData']);
+Route::post('submit-health-data',[HealthController::class,'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/manage', [JacketController::class, 'manage'])->middleware('admin');
