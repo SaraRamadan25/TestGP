4fc388839d94fd0a355be5a0903f1bf64ab2bc12
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2024-03-01 19:32:43 +0200

    Location to Pusher

--- app/Events/SensorDataReceived.php
+++ app/Events/SensorDataReceived.php
@@ -19,11 +19,15 @@ class SensorDataReceived implements ShouldBroadcast
      */
     public int $heartRate;
     public float $spo2;
+    public float $lat;
+    public float $lng;
 
-    public function __construct($heartRate, $spo2)
+    public function __construct($heartRate, $spo2, $latitude, $longitude)
     {
         $this->heartRate = $heartRate;
         $this->spo2 = $spo2;
+        $this->lat= $latitude;
+        $this->lng= $longitude;
     }
     public function broadcastOn(): Channel
     {
--- app/Http/Controllers/API/SensorController.php
+++ app/Http/Controllers/API/SensorController.php
@@ -12,15 +12,19 @@ class SensorController extends Controller
 {
     public function getData(Request $request): JsonResponse
     {
+        $latitude = $request->input('lat');
+        $longitude = $request->input('lng');
         $heartRate = $request->input('HeartRate');
         $spo2 = $request->input('Spo2');
 
         Log::info('Received sensor data', [
             'heartRate' => $heartRate,
             'spo2' => $spo2,
+            'latitude' => $latitude,
+            'longitude' => $longitude,
         ]);
 
-        broadcast(new SensorDataReceived($heartRate, $spo2))->toOthers();
+        broadcast(new SensorDataReceived($heartRate, $spo2, $latitude, $longitude))->toOthers();
 
         return response()->json(['message' => 'Data received successfully']);
     }
