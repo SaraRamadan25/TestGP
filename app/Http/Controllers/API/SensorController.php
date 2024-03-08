fe2608dd254aca523e1630ad817a4573d9599a45
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2024-03-08 13:51:02 +0200

    Test

--- app/Http/Controllers/API/SensorController.php
+++ app/Http/Controllers/API/SensorController.php
@@ -12,74 +12,62 @@ use Illuminate\Validation\ValidationException;
 
 class SensorController extends Controller
 {
-
     public function getData(Request $request): JsonResponse
     {
         $inputData = json_decode($request->getContent(), true);
 
-        if (isset($inputData['gpsData'])) {
-            $this->handleGpsData(json_decode($inputData['gpsData'], true));
+        $gpsData = json_decode($inputData['gpsData'], true);
+        $healthData = json_decode($inputData['healthData'], true);
+        $relayStatus = $inputData['relayStatus'];
+
+        try {
+            $validatedData = Validator::make($gpsData + $healthData + ['relayStatus' => $relayStatus], [
+                'lat' => 'sometimes|numeric',
+                'lng' => 'sometimes|numeric',
+                'heartRate' => 'sometimes|numeric',
+                'spo2' => 'sometimes|numeric',
+                'relayStatus' => 'sometimes|boolean',
+            ])->validate();
+
+            $latitude = $validatedData['lat'] ?? null;
+            $longitude = $validatedData['lng'] ?? null;
+            $heartRate = $validatedData['heartRate'] ?? null;
+            $spo2 = $validatedData['spo2'] ?? null;
+            $relayStatus = $validatedData['relayStatus'] ?? null;
+
+            Log::info('Received sensor data', [
+                'heartRate' => $heartRate,
+                'spo2' => $spo2,
+                'latitude' => $latitude,
+                'longitude' => $longitude,
+                'relayStatus' => $relayStatus,
+            ]);
+
+            broadcast(new SensorDataReceived($heartRate, $spo2, $latitude, $longitude, $relayStatus))->toOthers();
+
+            return response()->json(['message' => 'Data received successfully']);
+        } catch (ValidationException $e) {
+            return response()->json(['message' => 'Invalid data received', 'errors' => $e->errors()], 422);
         }
-
-        if (isset($inputData['healthData'])) {
-            $this->handleHealthData(json_decode($inputData['healthData'], true));
-        }
-
-        if (isset($inputData['relayStatus'])) {
-            $this->handleRelayStatus($inputData['relayStatus']);
-        }
-
-        return response()->json(['message' => 'Data received successfully']);
     }
-
-    private function handleGpsData(array $gpsData)
+    public function simulateSensorData(): JsonResponse
     {
-        $validatedData = Validator::make($gpsData, [
-            'lat' => 'sometimes|numeric',
-            'lng' => 'sometimes|numeric',
-        ])->validate();
+        $mockData = [
+            'gpsData' => json_encode([
+                'lat' => 51.509865,
+                'lng' => -0.118092
+            ]),
+            'healthData' => json_encode([
+                'heartRate' => 72,
+                'spo2' => 98
+            ]),
+            'relayStatus' => true
+        ];
 
-        $latitude = $validatedData['lat'] ?? null;
-        $longitude = $validatedData['lng'] ?? null;
+        $mockRequest = new Request([], [], [], [], [], [], json_encode($mockData));
 
-        Log::info('Received GPS data', [
-            'latitude' => $latitude,
-            'longitude' => $longitude,
-        ]);
-
-        broadcast(new SensorDataReceived(null, null, $latitude, $longitude, null))->toOthers();
+        return $this->getData($mockRequest);
     }
 
-    private function handleHealthData(array $healthData)
-    {
-        $validatedData = Validator::make($healthData, [
-            'heartRate' => 'sometimes|numeric',
-            'spo2' => 'sometimes|numeric',
-        ])->validate();
-
-        $heartRate = $validatedData['heartRate'] ?? null;
-        $spo2 = $validatedData['spo2'] ?? null;
-
-        Log::info('Received health data', [
-            'heartRate' => $heartRate,
-            'spo2' => $spo2,
-        ]);
-
-        broadcast(new SensorDataReceived($heartRate, $spo2, null, null, null))->toOthers();
-    }
-
-    private function handleRelayStatus($relayStatus)
-    {
-        $validatedData = Validator::make(['relayStatus' => $relayStatus], [
-            'relayStatus' => 'sometimes|boolean',
-        ])->validate();
-
-        $relayStatus = $validatedData['relayStatus'] ?? null;
-
-        Log::info('Received relay status', [
-            'relayStatus' => $relayStatus,
-        ]);
-
-        broadcast(new SensorDataReceived(null, null, null, null, $relayStatus))->toOthers();
-    }
 }
+
